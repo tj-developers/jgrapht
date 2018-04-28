00e983b73343cd64cd284fb6171b1dfabb1e4b6c
Ira Justus Fesefeldt <justus.fesefeldt@rwth-aachen.de>
2018-04-28 12:02:36 +0200

    added the last forbidden interval graph test added complete graph test

--- jgrapht-core/src/test/java/org/jgrapht/intervalgraph/interval/IntervalGraphRecognizerTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/intervalgraph/interval/IntervalGraphRecognizerTest.java
@@ -1,7 +1,9 @@
 package org.jgrapht.intervalgraph.interval;
 
-import org.jgrapht.Graph;
+import org.jgrapht.*;
 import org.jgrapht.alg.intervalgraph.IntervalGraphRecognizer;
+import org.jgrapht.alg.util.*;
+import org.jgrapht.generate.*;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 
@@ -11,6 +13,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import java.lang.reflect.*;
+import java.util.*;
 
 public class IntervalGraphRecognizerTest {
 
@@ -23,7 +28,8 @@ public class IntervalGraphRecognizerTest {
 
     @Test
     public void testForbiddenSubgraphBiclawWithAT() {
-        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder = new SimpleGraph<>((sourceVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
+        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder 
+            = new SimpleGraph<>((sourceVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
 
         builder.addEdge(0, 1);
         builder.addEdge(0, 2);
@@ -40,7 +46,8 @@ public class IntervalGraphRecognizerTest {
     public void testForbiddenSubgraphLekkerkerkerBoland() {
 
 
-        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder = new SimpleGraph<>((sourceVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
+        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder 
+            = new SimpleGraph<>((sourceVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
 
         builder.addEdge(0, 1);
         builder.addEdge(0, 2);
@@ -59,7 +66,8 @@ public class IntervalGraphRecognizerTest {
     }
 
     public void testForbiddenSubgraphLekkerkerkerBolandFamily(int n) {
-        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder = new SimpleGraph<>((sourceVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
+        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder 
+            = new SimpleGraph<>((sourceVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
 
         builder.addEdge(0, 1);
 
@@ -79,9 +87,41 @@ public class IntervalGraphRecognizerTest {
             testForbiddenSubgraphLekkerkerkerBolandFamily(n);
         }
     }
+    
+    
+    public void isForbiddenSubgraphLekkerkerkerBolandFamily2(int n) {
+        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder 
+            = new SimpleGraph<>((sourveVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
+        
+        //asteroidal triple: 1,3,5
+        builder.addEdge(0, 1);
+        builder.addEdge(1, 2);
+        builder.addEdge(2, 3);
+        builder.addEdge(3, 4);
+        builder.addEdge(4, 5);
+        builder.addEdge(5, n);
+        
+        for (int i=5; i<n; i++) {
+            builder.addEdge(i, i+1);
+            builder.addEdge(i, 2);
+            builder.addEdge(i, 4);
+        }
+        builder.addEdge(n, 2);
+        builder.addEdge(n, 4);
+        
+        assertFalse(IntervalGraphRecognizer.isIntervalGraph(builder.build()));
+    }
+    
+    @Test
+    public void testForbiddenSubgraphLekkerkerkerBolandFamily2() {
+        for(int n = 7; n < 20; n++) {
+            testForbiddenSubgraphLekkerkerkerBolandFamily(n);
+        }
+    }
 
     public boolean isCnAnIntervalGraph(int n) {
-        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder = new SimpleGraph<>((sourceVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
+        GraphBuilder<Integer, DefaultEdge, ? extends SimpleGraph<Integer, DefaultEdge>> builder 
+            = new SimpleGraph<>((sourceVertex, targetVertex) -> new DefaultEdge()).createBuilder(DefaultEdge.class);
 
         for(int i = 0; i < n; i++) {
             builder.addEdge(i, (i + 1) % n);
@@ -95,5 +135,25 @@ public class IntervalGraphRecognizerTest {
         for(int n = 2; n < 20; n++) {
             assertEquals("Testing C_" + n, n < 4, isCnAnIntervalGraph(n));
         }
+    }
+    
+    public void isCompleteAnIntervalGraph(int n) {
+        CompleteGraphGenerator<Integer,DefaultEdge> cgg = 
+            new CompleteGraphGenerator<Integer,DefaultEdge>(n);
+        Graph<Integer,DefaultEdge> cg =
+            new SimpleGraph<>(DefaultEdge.class);
+        cgg.generateGraph(cg, new IntegerVertexFactory(), null);
+        
+        //Every complete Graph is an interval graph
+        assertTrue(IntervalGraphRecognizer.isIntervalGraph(cg));
+    }
+    
+    @Test
+    public void completeGraphTest()
+    {
+        for(int i=0; i<20; i++) {
+            isCompleteAnIntervalGraph(i);
+        }
+            
     }
 }
