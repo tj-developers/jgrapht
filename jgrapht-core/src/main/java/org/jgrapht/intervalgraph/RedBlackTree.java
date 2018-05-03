a39a3aff98c5c04f1909c19b4238e7af67f0efa2
Daniel A. Mock <qwertz120@gmail.com>
2018-05-03 12:34:55 +0200

    Added list constructor

--- jgrapht-core/src/main/java/org/jgrapht/intervalgraph/RedBlackTree.java
+++ jgrapht-core/src/main/java/org/jgrapht/intervalgraph/RedBlackTree.java
@@ -1,9 +1,7 @@
 package org.jgrapht.intervalgraph;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 /**
  * Implementation of a Red-Black-Tree
@@ -24,6 +22,89 @@ public class RedBlackTree<K extends Comparable<K>, V> implements BinarySearchTre
     public Node<K, V> getRoot() {
         return root;
     }
+
+    public RedBlackTree() {
+    }
+
+
+    /**
+     * Construct a tree with the given keys and values.
+     * keys.get(i) is assigned to values.get(i)
+     * Runs in linear time if list is sorted, otherwise naive insertion is performed
+     * @param keys
+     * @param values
+     */
+    public RedBlackTree(List<K> keys, List<V> values) {
+        ArrayList<K> keyList;
+        ArrayList<V> valueList;
+        int length = keys.size();
+
+        // exceptions
+        if (length != values.size()) {
+            throw new RuntimeException("Key and value list have to have same length");
+        }
+        if (keys.size() == 0) {
+            return;
+        }
+
+        // transform to ArrayList for good performance
+        if (keys instanceof ArrayList) {
+            keyList = (ArrayList<K>) keys;
+        } else {
+            keyList = new ArrayList<>(keys);
+        }
+
+        if (values instanceof ArrayList) {
+            valueList = (ArrayList<V>) values;
+        } else {
+            valueList = new ArrayList<>(values);
+        }
+
+        // check if list is sorted to use efficient insertion
+        boolean isSorted = true;
+        K current = keyList.get(0);
+        for (int i = 1; i < length; i++) {
+            K next = keyList.get(i);
+            if (current.compareTo(next) > 0) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        // use optimized insert if input is sorted, otherwise trivial insertion
+        if (isSorted) {
+            root = sortedListToBST(keyList, valueList, 0, length - 1);
+        } else {
+            for (int i = 0; i < length; i++) {
+                this.insert(keyList.get(i), valueList.get(i));
+            }
+        }
+    }
+
+    private Node<K, V> sortedListToBST(ArrayList<K> keys, ArrayList<V> values, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = start + (end - start) / 2;
+        Node<K, V> node = new Node<>(keys.get(mid), values.get(mid), false, 0); // colors and size have to be updated
+        Node<K, V> left = sortedListToBST(keys, values, start, mid - 1);
+        Node<K, V> right = sortedListToBST(keys, values, mid + 1, end);
+        node.setLeftChild(left);
+        node.setRightChild(right);
+
+        // color all nodes black and only the leaves red
+        if (left == null && right == null) {
+            node.setRed();
+            node.setSize(0);
+        } else {
+            // update sizes
+            node.setSize(Math.max((left != null) ? left.getSize() : 0, right != null ? right.getSize() : 0) + 1);
+        }
+
+        return node;
+    }
+
 
     /**
      * Returns the value associated with the given key
--- jgrapht-core/src/test/java/org/jgrapht/intervalgraph/RedBlackTreeTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/intervalgraph/RedBlackTreeTest.java
@@ -3,6 +3,9 @@ package org.jgrapht.intervalgraph;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
@@ -11,72 +14,85 @@ import static org.junit.Assert.assertFalse;
  */
 public class RedBlackTreeTest {
 
-  private RedBlackTree<Integer, Integer> redBlackTree = new RedBlackTree<>();
+    private RedBlackTree<Integer, Integer> redBlackTree = new RedBlackTree<>();
 
-  @Before
-  public void setUp() {
-    redBlackTree.insert(13, 13);
-    redBlackTree.insert(8, 8);
-    redBlackTree.insert(17, 17);
-    redBlackTree.insert(1, 1);
-    redBlackTree.insert(11, 11);
-    redBlackTree.insert(15, 15);
-    redBlackTree.insert(25, 25);
-    redBlackTree.insert(6, 6);
-    redBlackTree.insert(22, 22);
-    redBlackTree.insert(27, 27);
-  }
-
-  /**
-   * The root is black
-   */
-  @Test
-  public void testBlackRoot() {
-    assertFalse(redBlackTree.getRoot().isRed());
-  }
-
-  /**
-   * If a node is red, then both its children are black
-   */
-  @Test
-  public void testBlackChildren() {
-    testBlackChildren(redBlackTree.getRoot());
-  }
-
-  private <K, V> void testBlackChildren(Node<K, V> currentNode) {
-    Node<K, V> leftChild = currentNode.getLeftChild();
-    if (leftChild != null) {
-      if (currentNode.isRed()) {
-        assertFalse(leftChild.isRed());
-      }
-      testBlackChildren(leftChild);
+    @Before
+    public void setUp() {
+        redBlackTree.insert(13, 13);
+        redBlackTree.insert(8, 8);
+        redBlackTree.insert(17, 17);
+        redBlackTree.insert(1, 1);
+        redBlackTree.insert(11, 11);
+        redBlackTree.insert(15, 15);
+        redBlackTree.insert(25, 25);
+        redBlackTree.insert(6, 6);
+        redBlackTree.insert(22, 22);
+        redBlackTree.insert(27, 27);
     }
 
-    Node<K, V> rightChild = currentNode.getRightChild();
-    if (rightChild != null) {
-      if (currentNode.isRed()) {
-        assertFalse(rightChild.isRed());
-      }
-      testBlackChildren(rightChild);
+    /**
+     * The root is black
+     */
+    @Test
+    public void testBlackRoot() {
+        assertFalse(redBlackTree.getRoot().isRed());
     }
-  }
 
-  /**
-   * Every path from a given node to any of its descendant leaves contains the same number of black nodes
-   */
-  @Test
-  public void testBlackNodeNumber() {
-    assertEquals(countLeftChildren(redBlackTree.getRoot(), 0), countRightChildren(redBlackTree.getRoot(), 0));
-  }
+    /**
+     * If a node is red, then both its children are black
+     */
+    @Test
+    public void testBlackChildren() {
+        testBlackChildren(redBlackTree.getRoot());
+    }
 
-  private <K, V> int countLeftChildren(Node<K, V> node, int currentBlackNumber) {
-    currentBlackNumber = node.isRed() ? currentBlackNumber : currentBlackNumber + 1;
-    return node.getLeftChild() == null ? currentBlackNumber : countLeftChildren(node.getLeftChild(), currentBlackNumber);
-  }
+    private <K, V> void testBlackChildren(Node<K, V> currentNode) {
+        Node<K, V> leftChild = currentNode.getLeftChild();
+        if (leftChild != null) {
+            if (currentNode.isRed()) {
+                assertFalse(leftChild.isRed());
+            }
+            testBlackChildren(leftChild);
+        }
 
-  private <K, V> int countRightChildren(Node<K, V> node, int currentBlackNumber) {
-    currentBlackNumber = node.isRed() ? currentBlackNumber : currentBlackNumber + 1;
-    return node.getRightChild() == null ? currentBlackNumber : countRightChildren(node.getRightChild(), currentBlackNumber);
-  }
+        Node<K, V> rightChild = currentNode.getRightChild();
+        if (rightChild != null) {
+            if (currentNode.isRed()) {
+                assertFalse(rightChild.isRed());
+            }
+            testBlackChildren(rightChild);
+        }
+    }
 
+    /**
+     * Every path from a given node to any of its descendant leaves contains the same number of black nodes
+     */
+    @Test
+    public void testBlackNodeNumber() {
+        assertEquals(countLeftChildren(redBlackTree.getRoot(), 0), countRightChildren(redBlackTree.getRoot(), 0));
+    }
+
+    private <K, V> int countLeftChildren(Node<K, V> node, int currentBlackNumber) {
+        currentBlackNumber = node.isRed() ? currentBlackNumber : currentBlackNumber + 1;
+        return node.getLeftChild() == null ? currentBlackNumber : countLeftChildren(node.getLeftChild(), currentBlackNumber);
+    }
+
+    private <K, V> int countRightChildren(Node<K, V> node, int currentBlackNumber) {
+        currentBlackNumber = node.isRed() ? currentBlackNumber : currentBlackNumber + 1;
+        return node.getRightChild() == null ? currentBlackNumber : countRightChildren(node.getRightChild(), currentBlackNumber);
+    }
+
+    @Test
+    public void testListConstructor() {
+        List<Integer> keyList = new LinkedList<>();
+        for (int i = 0; i < 20; i++) {
+            keyList.add(i);
+        }
+
+        RedBlackTree<Integer, Integer> tree = new RedBlackTree<>(keyList, keyList);
+        List<Integer> result = tree.inorderValues();
+        for (int i = 0; i < 20; i++) {
+            assertEquals(i, (int) result.get(i));
+        }
+    }
 }
