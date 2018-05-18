2ad8daac729c59ad89ae8fef6c3dab1350b791e1
unknown <christoph.gruene@rwth-aachen.de>
2018-05-18 21:58:14 +0200

    make ColorRefinementIsomorphismInspector extend RefinementAbstractIsomorpismInspector

--- jgrapht-core/src/main/java/org/jgrapht/alg/isomorphism/ColorRefinementIsomorphismInspector.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/isomorphism/ColorRefinementIsomorphismInspector.java
@@ -1,5 +1,6 @@
 package org.jgrapht.alg.isomorphism;
 
+import org.jgrapht.Graph;
 import org.jgrapht.GraphMapping;
 
 import java.util.Iterator;
@@ -8,9 +9,11 @@ import java.util.Iterator;
  * @param <V> the type of the vertices
  * @param <E> the type of the edges
  */
-public class ColorRefinementIsomorphismInspector<V, E> implements IsomorphismInspector<V, E> {
+public class ColorRefinementIsomorphismInspector<V, E> extends RefinementAbstractIsomorphismInspector<V, E> {
 
-
+    public ColorRefinementIsomorphismInspector(Graph<V, E> graph1, Graph<V, E> graph2) {
+        super(graph1, graph2);
+    }
 
     /**
      *
