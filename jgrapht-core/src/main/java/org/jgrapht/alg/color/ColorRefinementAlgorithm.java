df586b2c2445532592555ae2276e3a1486e28bd2
unknown <christoph.gruene@rwth-aachen.de>
2018-05-23 19:40:27 +0200

    finished implementation of efficient color refinement

--- jgrapht-core/src/main/java/org/jgrapht/alg/color/ColorRefinementAlgorithm.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/color/ColorRefinementAlgorithm.java
@@ -26,84 +26,196 @@ public class ColorRefinementAlgorithm<V, E> implements VertexColoringAlgorithm<V
     
     @Override
     public Coloring<V> getColoring() {
+        Integer n = graph.vertexSet().size();
+        Integer k; // number of colors used
+        Map<V, Integer> alpha = getAlpha(graph.vertexSet());
 
-        Map<V, Integer> vertexToColorMap = new HashMap<>(); // future coloring
-        Map<Integer, Set<V>> colorToVertexMap = new HashMap<>(); // mapping from color to all vertices with that color
-        Integer lastColor = 1; // last color used
-        Integer numberOfColorsUsed = 1; // number of colors used
+        Map<Integer, LinkedList<V>> C = new HashMap<>(); //initial colouring
+        Map<Integer, LinkedList<V>> A = new HashMap<>(); // mapping from color to all vertices with that color
 
-        Queue<Integer> colorQueue = new LinkedList<>(); // queue for all colors that will be used for refinement
-        colorQueue.add(1); // init queue with first color
+        Map<Integer, Integer> maxcdeg = new HashMap<>();
+        Map<Integer, Integer> mincdeg = new HashMap<>();
+        Map<V, Integer> cdeg = new HashMap<>();
+        Map<V, Integer> colour = new HashMap<>(); // future coloring
 
-        HashSet<V> color1VertexMap = new HashSet<>(); // helper set: vertex set for first color
-        Integer currentColor; // helper variable
-        Map<V, Integer> numberOfNeighborsForColor = new HashMap<>(); // helper map for loop
-        List<Set<V>> orderedPartition; // helper list for partition in loop
-
-        for(V vertex : graph.vertexSet()) {
-            vertexToColorMap.put(vertex, 1); // initial coloring
-            color1VertexMap.add(vertex); // map is initialized for first color
+        for(int c = 1; c <= n; ++c) {
+            C.put(c, new LinkedList<>()); //initial colouring
+            A.put(c, new LinkedList<>()); // mapping from color to all vertices with that color
+            maxcdeg.put(c, 0);
         }
-        colorToVertexMap.put(1, color1VertexMap); // initialize mapping form color to vertices
+        for(V v : graph.vertexSet()) {
+            C.get(alpha.get(v)).add(v);
+            cdeg.put(v, 0);
+            colour.put(v, alpha.get(v));
+        }
+        k = alpha.size(); // number of colors used
+        Stack<Integer> S_refine = getS_refine(alpha);
+        //S_refine.sort(Comparator.comparingInt(o -> o)); // sort stack ascendingly (not necessary with this implementation because getS_refine() already returns a sorted stack)
+        LinkedList<Integer> Colors_adj = new LinkedList<>();
 
-        while(!colorQueue.isEmpty()) {
-            currentColor = colorQueue.poll();
+        while(!S_refine.empty()) {
+            Integer r = S_refine.pop();
 
-            for(V vertex : graph.vertexSet()) {
-                numberOfNeighborsForColor.put(vertex, getNumberOfNeighborsOfColor(vertex, currentColor)); // number of neighbors of vertex color currentColor
-            }
-            orderedPartition = getOrderedPartition(vertexToColorMap, numberOfNeighborsForColor);
+            //calculate number of neighbours of v of colour r
+            calculateColourDegree(r, colour, C, A, Colors_adj, maxcdeg, mincdeg, cdeg);
 
-            if(orderedPartition.size() > numberOfColorsUsed) {
-                for(Integer color = lastColor - numberOfColorsUsed + 1; color <= lastColor; ++color) {
-                    Integer i1 = 0;
-                    Integer i2 = 0;
-                    getColorClassesOfColor(i1, i2, orderedPartition); // color class of c will be split into classes B_{i1}, ..., B_{i2}
+            //calculate new partition of the colors
+            calculatePartition(colour, C, A, S_refine, k, Colors_adj, maxcdeg, mincdeg, cdeg);
 
-                    Integer iStar = maximumPartitionSize(i1, i2, orderedPartition); // find the largest class
-
-                    addColorsToColorQueue(colorQueue, lastColor, i1, i2, iStar); // add all colors except the one with the largest class to queue colorQueue
+            //reset attributes for new iteration
+            for(Integer c : Colors_adj) {
+                for(V v : A.get(c)) {
+                    cdeg.put(v, 0);
                 }
+                maxcdeg.put(c, 0);
+                A.put(c, new LinkedList<>());
+                Colors_adj.remove(c);
+            }
 
-                for(Integer b = lastColor + 1; b <= lastColor + orderedPartition.size(); ++b) {
-                    saveNewColoring();
+        }
+
+        return new ColoringImpl<>(colour, colour.size());
+    }
+
+    /**
+     *
+     *
+     * @param r
+     * @param colour
+     * @param C
+     * @param A
+     * @param Colors_adj
+     * @param maxcdeg
+     * @param mincdeg
+     * @param cdeg
+     */
+    private void calculateColourDegree(Integer r, Map<V, Integer> colour, Map<Integer, LinkedList<V>> C, Map<Integer, LinkedList<V>> A, LinkedList<Integer> Colors_adj, Map<Integer, Integer> maxcdeg, Map<Integer, Integer> mincdeg, Map<V, Integer> cdeg) {
+        for(V v : C.get(r)) {
+            Set<E> N_minus = graph.incomingEdgesOf(v);
+            for(E e : N_minus) {
+                V w = graph.getEdgeTarget(e);
+                cdeg.put(w, cdeg.get(w) + 1);
+                if(cdeg.get(w) == 1) {
+                    A.get(colour.get(w)).add(w);
+                }
+                if(!Colors_adj.contains(colour.get(w))) {
+                    Colors_adj.add(colour.get(w));
+                }
+                if(cdeg.get(w) > maxcdeg.get(colour.get(w))) {
+                    maxcdeg.put(colour.get(w), cdeg.get(w));
                 }
             }
         }
 
-        return new ColoringImpl<>(vertexToColorMap, numberOfColorsUsed);
-    }
-
-    private Integer getNumberOfNeighborsOfColor(V vertex, Integer color) {
-        // TODO
-        return 0;
-    }
-
-    private List<Set<V>> getOrderedPartition(Map<V, Integer> coloring, Map<V, Integer> numberOfNeighborsForColor) {
-        List<Set<V>> partition = new ArrayList<>();
-        // TODO
-        return partition;
-    }
-
-    private void getColorClassesOfColor(Integer i1, Integer i2, List<Set<V>> orderedPartition) {
-        i1 = 1;
-        i2 = 1;
-        // TODO
-    }
-
-    private Integer maximumPartitionSize(Integer i1, Integer i2, List<Set<V>> orderedPartition) {
-        return 0;
-    }
-
-    private void addColorsToColorQueue(Queue<Integer> colorQueue, Integer lastColor, Integer i1, Integer i2, Integer iStar) {
-        for(Integer i = i1; i <= i2; ++i) {
-            if(!i.equals(iStar)) {
-                colorQueue.add(lastColor + i);
+        for(Integer c : Colors_adj) {
+            if(C.get(c).size() != A.get(c).size()) {
+                mincdeg.put(c, 0);
+            } else {
+                mincdeg.put(c, maxcdeg.get(c));
+                for(V v : A.get(c)) {
+                    if (cdeg.get(v) < mincdeg.get(c)) {
+                        mincdeg.put(c, cdeg.get(v));
+                    }
+                }
             }
         }
     }
 
-    private void saveNewColoring() {
-        // TODO
+
+    private void calculatePartition(Map<V, Integer> colour, Map<Integer, LinkedList<V>> C, Map<Integer, LinkedList<V>> A, Stack<Integer> S_refine, Integer k, LinkedList<Integer> Colors_adj, Map<Integer, Integer> maxcdeg, Map<Integer, Integer> mincdeg, Map<V, Integer> cdeg) {
+        LinkedList<Integer> Colors_split = new LinkedList<>(); // subset of Colors_adj (of colours) that will be split up
+        for(Integer c : Colors_adj) {
+            if(mincdeg.get(c) < maxcdeg.get(c)) {
+                Colors_split.add(c);
+            }
+        }
+        Colors_split.sort(Comparator.comparingInt(o -> o));
+        for(Integer s : Colors_split) {
+            SplitUpColour(s, colour, C, A, S_refine, k, maxcdeg, mincdeg, cdeg);
+        }
     }
+
+
+    private void SplitUpColour(Integer s, Map<V, Integer> colour, Map<Integer, LinkedList<V>> C, Map<Integer, LinkedList<V>> A, Stack<Integer> S_refine, Integer k, Map<Integer, Integer> maxcdeg, Map<Integer, Integer> mincdeg, Map<V, Integer> cdeg) {
+        Map<Integer, Integer> numcdeg = new HashMap<>();
+        Map<Integer, Integer> f = new HashMap<>();
+        boolean instack;
+
+        int maxcdeg_ = maxcdeg.get(s);
+
+        for(int i = 1; i <= maxcdeg_; ++i) {
+            numcdeg.put(i, 0);
+        }
+        numcdeg.put(0, C.get(s).size() - A.get(s).size());
+
+        for(V v : A.get(s)) {
+            numcdeg.put(cdeg.get(v), numcdeg.get(cdeg.get(v)) + 1);
+        }
+        int b = 0;
+        for(int i = 1; i <= maxcdeg_; ++i) {
+            if(numcdeg.get(i) > numcdeg.get(b)) {
+                b = i;
+            }
+        }
+        instack = S_refine.contains(s);
+
+        addColorsToS_refine(s, maxcdeg_, mincdeg, S_refine, k, numcdeg, f, instack, b);
+
+        for(V v : A.get(s)) {
+            if(f.get(cdeg.get(v)) != s) {
+                C.get(s).remove(v);
+                C.get(f.get(cdeg.get(v))).add(v);
+                colour.put(v, f.get(cdeg.get(v)));
+            }
+        }
+    }
+
+
+    private void addColorsToS_refine(Integer s, Integer maxcdeg_, Map<Integer, Integer> mincdeg, Stack<Integer> S_refine, Integer k, Map<Integer, Integer> numcdeg, Map<Integer, Integer> f, boolean instack, int b) {
+        for(int i = 0; i <= maxcdeg_; ++i) {
+            if(numcdeg.get(i) >= 1) {
+                if(i == mincdeg.get(s)) {
+                    f.put(i, s);
+                    if(!instack && b != i) {
+                        S_refine.push(f.get(i));
+                    }
+                } else {
+                    k++;
+                    f.put(i, k);
+                    if(instack && i != b) {
+                        S_refine.push(f.get(i));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     *
+     * @param vertices
+     * @return
+     */
+    private Map<V, Integer> getAlpha(Set<V> vertices) {
+        Map<V, Integer> alpha = new HashMap<>();
+        for(V v : vertices) {
+            alpha.put(v, 1);
+        }
+        return alpha;
+    }
+
+    /**
+     *
+     *
+     * @param alpha
+     * @return
+     */
+    private Stack<Integer> getS_refine(Map<V, Integer> alpha) {
+        Stack<Integer> S_refine = new Stack<>();
+        for(int i = alpha.size(); i > 0; --i) {
+            S_refine.push(i);
+        }
+        return S_refine;
+    }
+
 }
