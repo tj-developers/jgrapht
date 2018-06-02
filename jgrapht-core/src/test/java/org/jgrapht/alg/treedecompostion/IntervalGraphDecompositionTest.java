e4bcaa7a19ab16c38fa857d49ae6d0423a2d0040
Ira Justus Fesefeldt <ira@sutsuj.de>
2018-06-02 19:07:05 +0200

    ... and this

--- jgrapht-core/src/test/java/org/jgrapht/alg/treedecompostion/IntervalGraphDecompositionTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/treedecompostion/IntervalGraphDecompositionTest.java
@@ -3,7 +3,6 @@ package org.jgrapht.alg.treedecompostion;
 import static org.junit.Assert.*;
 
 import java.util.*;
-import java.util.Map.Entry;
 
 import org.jgrapht.*;
 import org.jgrapht.alg.treedecomposition.*;
@@ -14,89 +13,6 @@ import org.junit.*;
 public class IntervalGraphDecompositionTest
 {
 
-    private <V,E,W> void testNiceDecomposition(Graph<V,E> decomposition, Map<V,Set<W>> map, V root){
-        
-        Queue<V> queue = new LinkedList<V>();
-        
-        //test and add root
-        assertTrue(root+" is no valid root"
-                + "\n in decomposition "+decomposition
-                + "\n and map"+map, map.get(root).size() == 0);
-       queue.add(root);
-        
-        while(!queue.isEmpty())
-        {
-            V current = queue.poll();
-            List<V> successor = Graphs.successorListOf(decomposition, current);
-            if(successor.size() == 0 && map.get(current).isEmpty()) continue; //leaf node
-            if(successor.size() == 1) //forget or introduce
-            {
-                V next = successor.get(0);
-                queue.add(next);
-                Set<W> union = new HashSet<W>(map.get(current));
-                union.addAll(map.get(next));
-                if(union.size() == map.get(next).size() || union.size() == map.get(current).size())
-                {
-                    if(map.get(current).size() == map.get(next).size()-1) continue; //introduce
-                    else if(map.get(current).size()-1 == map.get(next).size()) continue; //forget
-                }
-            }
-            if(successor.size() == 2) //join
-            {
-                V first = successor.get(0);
-                V second = successor.get(1);
-                queue.add(first);
-                queue.add(second);
-                Set<W> union = new HashSet<W>(map.get(current));
-                union.addAll(map.get(first));
-                union.addAll(map.get(second));
-                if(union.size() == map.get(current).size() 
-                && union.size() == map.get(first).size() 
-                && union.size() == map.get(second).size()) 
-                    continue; //join node!
-            }
-            assertFalse("Vertex Set "+current+" is not a valid node for a nice decomposition"
-                + "\nin decomposition "+decomposition
-                + "\nwith map "+map, true); //no valid node!
-        }
-        assertTrue(true);
-    }
-    
-    private <V,E,W,F> void testDecomposition(Graph<W,F> oldGraph, Graph<V,E> decomposition, Map<V,Set<W>> map){
-    	Set<F> edgeSet = oldGraph.edgeSet();
-    	Set<V> vertexSet = decomposition.vertexSet();
-    	//Every edge is represented
-    	for(F e : edgeSet)
-    	{
-    		boolean hasVertex = false;
-    		for(V v : vertexSet)
-    		{
-    			if(map.get(v).contains(oldGraph.getEdgeSource(e)) && map.get(v).contains(oldGraph.getEdgeTarget(e))) {
-    				hasVertex = true;
-    				continue; 
-    			}
-    		}
-    		assertTrue("Edge "+e+" is not found"
-    		    + "\nin graph "+decomposition
-    		    + "\nwith map "+map, hasVertex);
-    	}
-    	//every vertex has non-empty connected set of vertex sets
-    	Set<W> oldVertexSet = oldGraph.vertexSet();
-    	for(W w : oldVertexSet)
-    	{
-    		Set<V> keySet = new HashSet<V>();
-    		for(Entry<V,Set<W>> entry : map.entrySet())
-    		{
-    			if(entry.getValue().contains(w))
-    				keySet.add(entry.getKey());
-    		}
-    		//not empty
-    		assertFalse("Vertex "+w+" is not represented\n in decomposition "+decomposition+"\n and map "+map, keySet.isEmpty());
-    		//connected
-    		Graph<V,E> subgraph = new AsSubgraph<V,E>(decomposition,keySet);
-    		assertTrue("Vertex "+w+" is not connected\n in decomposition "+decomposition+"\n and map "+map,GraphTests.isConnected(subgraph));
-    	}
-    }
     
     /**
      * Interval Graph representation of the graph to test:
@@ -132,8 +48,8 @@ public class IntervalGraphDecompositionTest
         assertNotNull("graph was detected as not an interval graph", decomp);
         
         //test for nice decomposition
-        testNiceDecomposition(decomp.getDecomposition(), decomp.getMap(), decomp.getRoot());
-        testDecomposition(g, decomp.getDecomposition(), decomp.getMap());
+        NiceDecompositionUtil.testNiceDecomposition(decomp.getDecomposition(), decomp.getMap(), decomp.getRoot());
+        NiceDecompositionUtil.testDecomposition(g, decomp.getDecomposition(), decomp.getMap());
     }
 
     @Test
@@ -182,7 +98,7 @@ public class IntervalGraphDecompositionTest
         Graph<Integer,DefaultEdge> decomp = decompalg.getDecomposition();
         Map<Integer,Set<Interval<Integer>>> map = decompalg.getMap();
         Integer root = decompalg.getRoot();
-        testNiceDecomposition(decomp,map,root);
+        NiceDecompositionUtil.testNiceDecomposition(decomp,map,root);
         
     }
 
