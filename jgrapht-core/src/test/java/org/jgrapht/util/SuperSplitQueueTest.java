9ff41fa974c91762e56dc82fc09c9f6f81edf00b
Ira Justus Fesefeldt <ira@sutsuj.de>
2018-08-03 09:47:17 +0200

    Removed Output from SuperSplitQueue

--- jgrapht-core/src/test/java/org/jgrapht/util/SuperSplitQueueTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/util/SuperSplitQueueTest.java
@@ -58,34 +58,23 @@ public class SuperSplitQueueTest {
     }
 
     @Test
-    public void asArray() {
-        SubSplitQueue q = SubSplitQueue.subSplitQueueFactory(10);
-        System.out.println(Arrays.toString(q.asArray()));
-    }
-
-    @Test
     public void split2() {
         SubSplitQueue q = SubSplitQueue.subSplitQueueFactory(10);
         assertEquals(10, q.getSize());
-        assertArrayEquals(q.asArray(), new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+        assertArrayEquals(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},q.asArray());
 
         SubSplitQueue split = q.split(new int[] {2,5,9});
-        assertArrayEquals(q.asArray(), new int[] {0, 1, 3, 4, 6, 7, 8});
-        assertArrayEquals(split.asArray(), new int[] {2,5,9});
+        assertArrayEquals(new int[] {0, 1, 3, 4, 6, 7, 8},q.asArray());
+        assertArrayEquals(new int[] {2,5,9},split.asArray());
 
         SubSplitQueue splitsplit = split.split(new int[] {2});
         assertEquals(7, q.getSize());
         assertEquals(2, split.getSize());
         assertEquals(1, splitsplit.getSize());
 
-
-        for (int i : split) {
-            System.out.println(i);
-        }
-
-        assertArrayEquals(q.asArray(), new int[] {0, 1, 3, 4, 6, 7, 8});
-        assertArrayEquals(split.asArray(), new int[] {5, 9});
-        assertArrayEquals(splitsplit.asArray(), new int[] {2});
+        assertArrayEquals(new int[] {0, 1, 3, 4, 6, 7, 8}, q.asArray());
+        assertArrayEquals(new int[] {5, 9}, split.asArray());
+        assertArrayEquals(new int[] {2}, splitsplit.asArray());
     }
 
     @Test
@@ -94,39 +83,7 @@ public class SuperSplitQueueTest {
         Iterator<Integer> it = q.iterator();
         for (int i = 0; i < 10; i++) {
             int element =  it.next();
-
-            System.out.println(element);
             assertEquals(i, element);
-        }
-
-        SubSplitQueue split = q.split(new int[] {2,5,9});
-        for (int i: split) {
-            System.out.println(i);
-        }
-        for (int i: q) {
-            System.out.println(i);
-        }
-
-        System.out.println("...");
-    }
-
-    @Test
-    public void name() {
-        SubSplitQueue q = SubSplitQueue.subSplitQueueFactory(10);
-        SubSplitQueue split = q.split(new int[] {2,5,9});
-        //SubSplitQueue empty = split.split(new int[] {1});
-        //assertTrue(empty.isEmpty());
-
-        SubSplitQueue splitSplit = split.split(new int[] {2,5,9});
-
-        for (int i: splitSplit) {
-            System.out.println(i);
-        }
-
-        System.out.println("...");
-
-        for (int i: split) {
-            System.out.println(i);
         }
     }
 }
